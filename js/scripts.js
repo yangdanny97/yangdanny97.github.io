df1eae27a0015ab8b98fc27f7bb33d10145bee33
Danny Yang <dzy4@cornell.edu>
2017-04-07 01:42:46 -0400

    update to UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,61 @@
 
 var projectslist = ["p1","p2","p3","p4"];
+
+var details = {p1: "<a href='https://http://cornellcuprobotics.com/' target='_blank'><p class='details unselectable'>[ Python, Java, Javascript ] [ Jan. 2017 - present ]</p><p class='details unselectable'>Worked on the software platform for Minibot, an educational robotics platform being developed by Cornell Cup</p><p class='details unselectable'>Contributed to: Python API for custom scripts, Minibot simulator with JBox2D physics backend, and GUI for controlling the Minibot/simulator.</p></a>",
+p2: "<a href='https://devpost.com/software/proton-positivity-generator' target='_blank'><p class='details unselectable'>[ Python, Javascript ] [ Jan. 2017 ]</p><p class='details unselectable'>Web app with custom interface and skills for Amazon Alexa</p><p class='details unselectable'>Original compliment generation using Markov Chains</p><p class='details unselectable'>Eliminated the Alexa wake word for smoother UX</p></a>",
+p3: "<a href='https://github.com/yangdanny97/CoopMaster' target='_blank'><p class='details unselectable'>[ Java, Android Studio ] [ Jan. - Feb. 2017 ]</p><p class='details unselectable'>Data collection and visualization tool for backyard hen owners</p><p class='details unselectable'>Graphs trends in flock over time to track flock health</p><p class='details unselectable'>Finalist at Cornell Animal Health Hackathon, released on app store in Feb. 2017</p></a>",
+p4: "<a href='https://devpost.com/software/fun-with-foursquare' target='_blank'><p class='details unselectable'>[ Java, Javascript, Android Studio ] [ Oct. 2016 ]</p><p class='details unselectable'>Android app built with FourSquare API and Wikitude</p><p class='details unselectable'>Uses AR to display icons pointing to nearby locations that are trending on Foursquare</p></a>"};
+
+var original = {p1: "<div class='text-center'><h4><a href='https://http://cornellcuprobotics.com/' target='_blank'>Cornell Cup <img src='images/cup.png' width='60px' alt='Cup logo'/></a></h4><p class='unselectable'>Minibot Platform Team</p></div>",
+p2: "<div class='text-center'><h4 class='unselectable'><a href='https://devpost.com/software/proton-positivity-generator' target='_blank'>Proton<img src='images/alexa.png' width='80px' alt='Alexa Logo'/></a></h4><p class='unselectable'>HackDavis 2017 Honorable Mention</p></div>",
+p3: "<div class='text-center'><h4 class='unselectable'><a href='https://github.com/yangdanny97/CoopMaster' target='_blank'>CoopMaster <img src='images/coopmasterlogo.png' width='70px' alt='Alexa Logo'/></a></h4><p class='unselectable'>Animal Health Hackathon Finalist</p></div>",
+p4: "<div class='text-center'><h4 class='unselectable'><a href='https://devpost.com/software/fun-with-foursquare' target='_blank'>FunSquare <img src='images/funsquare.png' width='60px' alt='Funsquare Logo'/></a></h4><p class='unselectable'>HackNY 2016 First Place</p></div>"};
+
 var active = null;
 
 function setActive(number){
-	var projects = [1,2,3,4];
+	var projects = ["p1","p2","p3","p4"];
 	projects.splice(number-1,1);
 	projects.forEach(function(e){
-		var id='p'+e.toString();
-		document.getElementById(id).innerHTML = original[e-1];
+		document.getElementById(e).innerHTML = original[e];
 		});
-	document.getElementById(projectslist[number-1]).innerHTML = details[number-1];
+	document.getElementById(projectslist[number-1]).innerHTML = details["p"+number.toString()];
 	active = projectslist[number-1];
 }
 
 function toggleActive(number){
 	if (active == projectslist[number-1]){
-		console.log(number);
-		document.getElementById(projectslist[number-1]).innerHTML = original[number-1];
+		document.getElementById(projectslist[number-1]).innerHTML = original["p"+number.toString()];
 		active = null;
 	} else {
-		var projects = [1,2,3,4];
-		projects.splice(number-1,1);
-		projects.forEach(function(e){
-			var id='p'+e.toString();
-			document.getElementById(id).innerHTML = original[e-1];
-		});
-		document.getElementById(projectslist[number-1]).innerHTML = details[number-1];
-		active = projectslist[number-1];
+        var projects = ["p1","p2","p3","p4"];
+        projects.splice(number-1,1);
+        projects.forEach(function(e){
+            document.getElementById(e).innerHTML = original[e];
+            });
+        document.getElementById(projectslist[number-1]).innerHTML = details["p"+number.toString()];
+        active = projectslist[number-1];
 	}		
 }
+
+document.addEventListener("DOMContentLoaded", function() { 
+  projectslist.forEach(function(e){
+    document.getElementById(e).innerHTML = original[e];
+  	document.getElementById(e).addEventListener('mouseenter',function() {
+        setActive(e.substr(1));
+    });
+    document.getElementById(e).addEventListener('click',function() {
+        toggleActive(e.substr(1));
+    });
+  });
+});
+
+document.getElementById('projects').addEventListener('mouseleave',function() {
+        active = null;
+        projectslist.forEach(function(e){
+        	document.getElementById(e).innerHTML = original[e];
+        });             
+    });
 
 document.getElementById('about-btn').addEventListener('click',function() {
         $('html, body').animate({
@@ -47,40 +74,4 @@ document.getElementById('cornell-cup').addEventListener('click',function() {
             scrollTop: $("#projects-marker").offset().top - 50
         }, 600)
     });
-
-document.addEventListener("DOMContentLoaded", function() { 
-  projectslist.forEach(function(e){
-  	document.getElementById(e).addEventListener('mouseenter',function() {
-        setActive(projectslist.indexOf(e)+1);
-    });
-    document.getElementById(e).addEventListener('click',function() {
-    	console.log("toggle");
-        toggleActive(projectslist.indexOf(e)+1);
-    });
-  });
-});
-
-
-document.getElementById('projects').addEventListener('mouseleave',function() {
-        var i = 0;
-        active = null;
-        projectslist.forEach(function(e){
-        	document.getElementById(e).innerHTML = original[i];
-        	i+=1; 
-        });             
-    });
-
-
-var details = ["<p class='details'>[ Python, Java, Javascript ] [ Jan. 2017 - present ]</p><p class='details'>Worked on the software platform for Minibot, an educational robotics platform being developed by Cornell Cup</p><p class='details'>Contributed to: Python API for custom scripts, Minibot simulator with JBox2D physics backend, and GUI for controlling the Minibot/simulator.</p>",
-"<p class='details'>[ Python, Javascript ] [ Jan. 2017 ]</p><p class='details'>Web app with custom interface and skills for Amazon Alexa</p><p class='details'>Original compliment generation using Markov Chains</p><p class='details'>Eliminated the Alexa wake word for smoother UX</p>",
-"<p class='details'>[ Java, Android Studio ] [ Jan. 2017 - Feb. 2017 ]</p><p class='details'>Data collection and visualization tool for owners of backyard hen flocks like my parents</p><p class='details'>Graphs behavioral trends in flock over time to track flock health</p><p class='details'>Finalist at Cornell Animal Health Hackathon, released on app store in Feb. 2017</p>",
-"<p class='details'>[ Java, Javascript, Android Studio ] [ Oct. 2016 ]</p><p class='details'>Android app built with FourSquare API and Wikitude</p><p class='details'>Uses AR to display icons pointing to nearby locations that are trending on Foursquare</p>"];
-
-var original = ["<div class='text-center'><h4>Cornell Cup <img src='images/cup.png' width='60px' alt='Cup logo'/></h4><p>Minibot Platform Team</p></div>",
-"<div class='text-center'><h4><a href='https://devpost.com/software/proton-positivity-generator' target='_blank'>Proton<img src='images/alexa.png' width='80px' alt='Alexa Logo'/></a></h4><p>HackDavis 2017 Honorable Mention</p></div>",
-"<div class='text-center'><h4><a href='https://github.com/yangdanny97/CoopMaster' target='_blank'>CoopMaster <img src='images/coopmasterlogo.png' width='70px' alt='Alexa Logo'/></a></h4><p>Animal Health Hackathon Finalist</p></div>",
-"<div class='text-center'><h4><a href='https://devpost.com/software/fun-with-foursquare' target='_blank'>FunSquare <img src='images/funsquare.png' width='60px' alt='Funsquare Logo'/></a></h4><p>HackNY 2016 First Place</p></div>"];
-
-
-
 
